886e59083ea20abcdb0c2c21fdc2b38f1612264f
Leo <582970957@qq.com>
2018-04-11 16:44:01 +0800

    init

---
+++ src/store/getters.js
@@ -0,0 +1,11 @@
+const getters = {
+  sidebar: state => state.app.sidebar,
+  visitedViews: state => state.tagsView.visitedViews,
+  cachedViews: state => state.tagsView.cachedViews,
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name,
+  roles: state => state.user.roles,
+  errorLogs: state => state.errorLog.logs
+}
+export default getters
